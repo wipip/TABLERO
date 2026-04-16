004de78da2aba3d24976d506ca7428335aecfd80
wipip <166628878+wipip@users.noreply.github.com>
2026-04-16 06:53:09 -0500

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(
-    page_title="Analizador imagenes IA",
+    page_title="Tablero de dibujo libre",
     page_icon="📝",
     layout="wide",
     initial_sidebar_state="expanded"
